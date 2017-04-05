13dfca861c4122e49f500c631fe1c94395788e25
Jared <lotrfreak95@yahoo.com>
2017-04-05 15:56:19 -0500

    Add files via upload

--- MachineProblem2/mp2_part7.cpp
+++ MachineProblem2/mp2_part7.cpp
@@ -2,7 +2,7 @@
 /* Developer: Andrew Kirfman                                                  */
 /* Project: CSCE-313 Machine Problem #3                                       */
 /*                                                                            */
-/* File: ./mp3_part1.cpp                                                      */
+/* File: ./mp3_part7.cpp                                                      */
 /* -------------------------------------------------------------------------- */
 
 /* -------------------------------------------------------------------------- */
@@ -10,22 +10,15 @@
 /* -------------------------------------------------------------------------- */
 
 #include <stdlib.h>
-#include <iostream>
-#include <sys/types.h>
-#include <string.h>
-#include <errno.h>
-#include <sys/wait.h>
 #include <unistd.h>
-using namespace std;
-int main(int argc, char **argv)
+
+
+int main()
 {
-    int pid = fork();
-    if (pid == 0) {
-        execl("bin/ls", "/bin/ls", "-la", NULL);
-    } else if (pid == -1) {
-        cout << "Error: Fork failed: " << strerror(errno) << endl;
-    } else {
-        waitpid(pid, NULL, 0);
-    }
+
+
+
+
     exit(0);
 }
+
