a35bf68b2105e7be6845201cbfe944849f809737
Jared <lotrfreak95@yahoo.com>
2017-04-05 16:01:06 -0500

    Add files via upload

---
+++ MachineProblemX/my_allocator.c
@@ -0,0 +1,79 @@
+/*
+    File: my_allocator.c
+
+    Author: <your name>
+            Department of Computer Science
+            Texas A&M University
+    Date  : <date>
+
+    Modified: 
+
+    This file contains the implementation of the module "MY_ALLOCATOR".
+
+*/
+
+/*--------------------------------------------------------------------------*/
+/* DEFINES */
+/*--------------------------------------------------------------------------*/
+
+    /* -- (none) -- */
+
+/*--------------------------------------------------------------------------*/
+/* INCLUDES */
+/*--------------------------------------------------------------------------*/
+
+#include<stdlib.h>
+#include "my_allocator.h"
+
+/*--------------------------------------------------------------------------*/
+/* DATA STRUCTURES */ 
+/*--------------------------------------------------------------------------*/
+
+    /* -- (none) -- */
+
+/*--------------------------------------------------------------------------*/
+/* CONSTANTS */
+/*--------------------------------------------------------------------------*/
+
+    /* -- (none) -- */
+
+/*--------------------------------------------------------------------------*/
+/* FORWARDS */
+/*--------------------------------------------------------------------------*/
+
+    /* -- (none) -- */
+
+/*--------------------------------------------------------------------------*/
+/* FUNCTIONS FOR MODULE MY_ALLOCATOR */
+/*--------------------------------------------------------------------------*/
+
+/* Don't forget to implement "init_allocator" and "release_allocator"! */
+
+
+Addr my_malloc(unsigned int _length) {
+  /* This preliminary implementation simply hands the call over the 
+     the C standard library! 
+     Of course this needs to be replaced by your implementation.
+  */
+  return malloc((size_t)_length);
+}
+
+int my_free(Addr _a) {
+  /* Same here! */
+  free(_a);
+  return 0;
+}
+
+void print_list(){
+
+}
+Addr buddy_address(Addr block_address){
+
+}
+bool split_block(Addr start_address){
+  return true;;
+}
+
+bool combine_blocks(Addr start_address1, Addr start_address2){
+  return true;;
+}
