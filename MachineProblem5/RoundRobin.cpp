e9a631d83e80e54593859da2c319302f45ff5ae1
Jared <lotrfreak95@yahoo.com>
2017-04-06 13:31:35 -0500

    Update RoundRobin.cpp

--- MachineProblem5/RoundRobin.cpp
+++ MachineProblem5/RoundRobin.cpp
@@ -1,19 +1,10 @@
 #include "RoundRobin.h"
 using namespace std;
 
-//No need to change it
 RoundRobin::RoundRobin() {
     time_quantum = 0;
 }
 
-
-/*
- This is a constructor for RoundRobin Scheduler, you should use the extractProcessInfo function first
- to load process information to process_info and then sort process by arrival time;
- Also, fill in the procesVec with shared_ptr
- 
- Also initialize time_quantum
- */
 RoundRobin::RoundRobin(string file, int quantum) {
     extractProcessInfo(file);
     set_time_quantum(quantum);
@@ -38,7 +29,6 @@ int RoundRobin::get_time_quantum() {
 
 //Schedule tasks based on RoundRobin Rule
 //the jobs are put in the order the arrived
-//Make sure you print out the information like we put in the document
 void RoundRobin::schedule_tasks() {
     int sys_time = 0;
     while(processVec.size() != 0) {
@@ -67,8 +57,6 @@ void RoundRobin::schedule_tasks() {
     }
 }
 
-//Fill in the procesVec with shared_ptr
-//And set time_quantum, or you can also use set_time_quantum function for setting quantum
 RoundRobin::RoundRobin(vector<ProcessInfo> &process_info, int quantum){
     set_time_quantum(quantum);
     for(auto elem : process_info) {
